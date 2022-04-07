6e3d14cc3bfd5eb964c1f2722deab950f54258e5
nullFoo <38643612+nullFoo@users.noreply.github.com>
2022-04-07 17:10:29 +1200

    stock price calculation (placeholder)

--- src/StocksAPI.js
+++ src/StocksAPI.js
@@ -3,5 +3,16 @@
 module.exports = {
 	GetStockInfo: (code) => {
         
+        let memberCount = 10; // todo, be the actual member count
+        if(memberCount < 10) {
+            return false;
+        }
+        else {
+            let totalStocksOwned = 10; // todo be real
+
+            let stockPrice = memberCount + (totalStocksOwned * 0.1);
+
+            return stockPrice;
+        }
     }
 };
