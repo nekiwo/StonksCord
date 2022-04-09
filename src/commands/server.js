441acfaac54253f33925ae112d02db4a1ed2a1af
nullFoo <38643612+nullFoo@users.noreply.github.com>
2022-04-09 15:50:46 +1200

    Create server.js

---
+++ src/commands/server.js
@@ -0,0 +1,34 @@
+const {SlashCommandBuilder} = require("@discordjs/builders");
+const {InviteToGuild} = require("../helpers");
+
+// just a testing/debug command
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("server")
+		.setDescription("Test command")
+        .addStringOption(option =>
+            option.setName("code")
+                .setDescription("Stock code or server link")
+                .setRequired(true)),
+	async execute(interaction) {
+        if (interaction) {
+            const stringInput = interaction.options.getString("code");
+            console.log(stringInput)
+
+            let guild;
+
+            guild = InviteToGuild(stringInput.substring(stringInput.lastIndexOf("/") + 1));
+            console.log(guild)
+
+            const embed = new MessageEmbed()
+                .setColor("#03fc5e")
+                .setTitle(guild.name)
+                .setThumbnail("https://via.placeholder.com/128")
+                .setDescription(`a`)
+                .toJSON();
+
+            return interaction.reply({embeds: [embed]});
+        }
+	},
+};
