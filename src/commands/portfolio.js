945efe0ecc52d0e023cd868a09f249b880b1fb2b
TheMemedOne <58634064+TheMemedOne@users.noreply.github.com>
2022-04-16 22:22:18 -0500

    check if not found

--- src/commands/portfolio.js
+++ src/commands/portfolio.js
@@ -10,23 +10,29 @@ module.exports = {
         if (interaction) {
             let userInfo = await GetUserInfo(interaction.user.id);
 
-            let userEmbed = new MessageEmbed()
-                .setColor("#03fc5e")
-                .setTitle(`User info for @${interaction.user.username}`)
-                .addFields({
-                    name: "Balance",
-                    value: userInfo.Balance.toString(),
-                    inline: true
-                }, {
-                    name: "Worth",
-                    value: stockInfo.MarketCap.toString(),
-                    inline: true
-                }, {
-                    name: "List of Stocks",
-                    value: "$TEST - 23 shares (4354$)\n$TEST - 23 shares (4354$)"
-                });
+            if (userInfo !== {}) {
+                console.log(userInfo)
+
+                let userEmbed = new MessageEmbed()
+                    .setColor("#03fc5e")
+                    .setTitle(`User info for @${interaction.user.username}`)
+                    .addFields({
+                        name: "Balance",
+                        value: userInfo.Balance.toString(),
+                        inline: true
+                    }, {
+                        name: "Worth",
+                        value: stockInfo.MarketCap.toString(),
+                        inline: true
+                    }, {
+                        name: "List of Stocks",
+                        value: "$TEST - 23 shares (4354$)\n$TEST - 23 shares (4354$)"
+                    });
             
-            return interaction.reply({embeds: [userEmbed.toJSON()]});
+                return interaction.reply({embeds: [userEmbed.toJSON()]});
+            } else {
+                // create user
+            }
         }
 	},
 };
