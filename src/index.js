e26f71a556a1a6f04620d632241e364f67954ec8
TheMemedOne <58634064+TheMemedOne@users.noreply.github.com>
2022-04-23 15:58:00 -0500

    fixed circurlar dependency

--- src/index.js
+++ src/index.js
@@ -2,18 +2,14 @@ const fs = require("fs");
 const {Client, Collection, Intents} = require("discord.js");
 const {Token} = require("./config.json");
 
-module.exports = {
-    client: new Client({intents: [Intents.FLAGS.GUILDS]})
-}
-
-const client = module.exports.client;
+const client = new Client({intents: [Intents.FLAGS.GUILDS]});
 
 client.commands = new Collection();
 const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
-	client.commands.set(command.data.name, command);
+    client.commands.set(command.data.name, command);
 }
 
 client.once("ready", () => {
@@ -28,7 +24,7 @@ client.on("interactionCreate", async interaction => {
 	if (!command) return;
 
 	try {
-		await command.execute(interaction);
+		await command.execute(interaction, client);
 	} catch (error) {
 		console.error(error);
 		return interaction.reply({content: "Sorry, there has been an error", ephemeral: true});
@@ -38,8 +34,9 @@ client.on("interactionCreate", async interaction => {
 client.on("guildCreate", guild => {
     const channels = guild.channels.cache.filter(channel => channel.type == "text");
 
-    channels.first().send("Hello! Start by using the `/help` command. Then, use the `/configure` command in order to put your server on the stonk market").catch(e => console.log(e));
+    channels.first()
+        .send("Hello! Start by using the `/help` command. Then, use the `/configure` command in order to put your server on the stonk market")
+        .catch(e => console.log(e));
 });
 
 client.login(Token);
-
