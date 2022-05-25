c500ac98b18038009959324a2c5ccd6eb44ae508
nullFoo <38643612+nullFoo@users.noreply.github.com>
2022-05-26 09:21:08 +1200

    skeleton for scoreboard command

--- src/commands/scoreboard.js
+++ src/commands/scoreboard.js
@@ -3,10 +3,30 @@ const {SlashCommandBuilder} = require("@discordjs/builders");
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName("scoreboard")
-		.setDescription("Top best/worst performing stocks and users"),
+		.setDescription("Top best/worst performing stocks and users")
+        .addStringOption(option => option
+            .setName("scoreboard")
+            .setDescription("Which scoreboard (stocks, stocksworst, users)")
+            .setRequired(true)
+        ),
 	async execute(interaction) {
         if (interaction) {
-            
+            const scoreboardType = interaction.options.getString("scoreboard");
+
+			switch (scoreboardType) {
+				case "stocks":
+					
+					break;
+				case "stocksworst":
+					
+					break;
+				case "users":
+					
+					break;
+				default:
+					return interaction.reply("Scoreboard type not found in list (stocks, stocksworst, users)");
+					break;
+			}
         }
 	},
 };
