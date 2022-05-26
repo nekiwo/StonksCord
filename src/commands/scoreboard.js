49cfe461a859e998ee45ed888257992acd0044c6
nekiwo <geo.panchev@gmail.com>
2022-05-26 10:33:45 -0500

    Workng on scoreboard

--- src/commands/scoreboard.js
+++ src/commands/scoreboard.js
@@ -16,17 +16,41 @@ module.exports = {
         if (interaction) {
             const scoreboardType = interaction.options.getString("scoreboard");
 
+            let finalEmbed;
+            let placeFormat;
 			switch (scoreboardType) {
 				case "BestStocks":
-					
+					finalEmbed = new MessageEmbed()
+                        .setColor("#03fc5e")
+                        .setTitle("Top 10 best performing stonks");
+
+                    placeFormat = (i) => {
+                        finalEmbed.addField(`#${i} $${bestStocks[i - 1].Code.toUpperCase()} - ${bestStocks[i - 1].Change}% 📈`, "", false);
+                    }
 					break;
 				case "WorstStocks":
-					
+					finalEmbed = new MessageEmbed()
+                        .setColor("#03fc5e")
+                        .setTitle("Top 10 worst performing stonks");
+
+                    placeFormat = (i) => {
+                        finalEmbed.addField(`#${i} $${worstStocks[i - 1].Code.toUpperCase()} - ${worstStocks[i - 1].Change}% 📉`, "", false);
+                    }
 					break;
 				case "RichestUsers":
-					
+					finalEmbed = new MessageEmbed()
+                        .setColor("#03fc5e")
+                        .setTitle("Top 10 richest users");
+
+                    placeFormat = (i) => {
+                        finalEmbed.addField(`#${i} $${richestUsers[i - 1].Tag} - ${richestUsers[i - 1].Worth}$`, "", false);
+                    }
 					break;
 			}
+
+            for (let i = 1; i <= 10; i++) {
+                placeFormat(i);
+            }
         }
 	}
 };
