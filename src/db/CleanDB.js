aaa973cf509ea2f4e25df715a8b3b5d96df57b5c
TheMemedOne <58634064+TheMemedOne@users.noreply.github.com>
2022-05-27 17:31:03 -0500

    still working on clean script

--- src/db/CleanDB.js
+++ src/db/CleanDB.js
@@ -1,9 +1,15 @@
-const {GetAllStocksData, GetAllStockMembersData} = require("./db");
+const {GetAllStocksData, GetAllStockMembersData, DeleteStockMembersData} = require("./db");
 
+// I just don't know how to make a SQL query to clean the database, so here's a script 
 GetAllStocksData().then(data => {
     data.forEach(stock => {
         let stockBuffer = {};
-        let stockResult = {};
+        let stockResult = {
+            members: [],
+            price: [],
+            total_shares: [],
+            time_stamps: []
+        };
 
         stock.time_stamps.forEach(timestamp => {
             let parsedTimestamp = new Date(date).toISOString().split("T")[0] + "T00:00:00.000Z";
@@ -15,11 +21,31 @@ GetAllStocksData().then(data => {
         });
 
         for (let date in stockBuffer) {
-            if (new Date(date).getTime() > "7 DAYS") {
-                // get max, push to array
+            if (Date.now() - new Date(date).getTime() > 604800000) { // 7 days
+                let max = Math.max(...stockBuffer[date]);
+                let maxIndex = stock.time_stamps.indexOf(max);
+
+                stockResult.members.push(stock.members[maxIndex]);
+                stockResult.total_shares.push(stock.total_shares[maxIndex]);
+                stockResult.price.push(stock.price[maxIndex]);
+                stockResult.time_stamps.push(max);
             } else {
-                // add arrays
+                stockBuffer[date].forEach(timestamp => {
+                    let timestampIndex = stock.time_stamps.indexOf(timestamp);
+
+                    stockResult.members.push(stock.members[timestampIndex]);
+                    stockResult.total_shares.push(stock.total_shares[timestampIndex]);
+                    stockResult.price.push(stock.price[timestampIndex]);
+                    stockResult.time_stamps.push(timestamp);
+                });
             }
         }
+
+        // replace arrays in the stock
+        DeleteStockMembersData(stock.id);
+        
+
+        // delete message timestamps older than 7 days in "stockCODE"
+
     });
 });
