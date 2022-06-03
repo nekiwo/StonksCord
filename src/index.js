239a3e0dddfb2300134a06749e23ffa5555d7846
TheMemedOne <58634064+TheMemedOne@users.noreply.github.com>
2022-06-03 18:40:28 -0500

    commit number for debugging in console

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ for (const file of commandFiles) {
 }
 
 client.once("ready", () => {
-	console.log("Ready!");
+	console.log("Ready! c150");
 });
 
 client.on("interactionCreate", async interaction => {
