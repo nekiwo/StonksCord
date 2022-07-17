98871ecde34f8374c05f89878cfb1c1c404be5ec
TheMemedOne <58634064+TheMemedOne@users.noreply.github.com>
2022-07-17 12:46:23 -0500

    remove commit number (unecessary)

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ for (const file of commandFiles) {
 }
 
 client.once("ready", () => {
-	console.log("Ready! c153");
+	console.log("Ready!");
 });
 
 client.on("interactionCreate", async interaction => {
